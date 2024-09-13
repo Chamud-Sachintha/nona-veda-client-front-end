a9870657985d711353083524b52a669bf7d82839
Chamud-Sachintha <sheraanmario777@gmail.com>
2024-09-13 08:32:38 +0530

    initla commit with modules

---
+++ src/app/layouts/web-layout/web-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './web-layout.component.html',
+  styleUrl: './web-layout.component.css'
+})
+export class WebLayoutComponent {
+
+}
---
+++ src/app/modules/web-module/web-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WebModuleRoutingModule } from './web-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WebModuleRoutingModule
+  ]
+})
+export class WebModuleModule { }
